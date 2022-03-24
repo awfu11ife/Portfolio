6714179b60c3975dd2ed31627479a3fc4fa3d6b9
awfu11ife <legj2013@yandex.ru>
2022-03-25 01:07:19 +0300

    Enemy
    Implementation of the enemy

---
+++ Enemy/StateMachine/Transitions/TargetDieTransition.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDieTransition : Transition
+{  
+    void Update()
+    {
+        if (Target == null)
+            NeedTransit = true;
+    }
+}
